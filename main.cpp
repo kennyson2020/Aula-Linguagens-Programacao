b09ac2489b6f9bd5f86c29c7324785f617690fa5
kennyson2020 <162520955+kennyson2020@users.noreply.github.com>
2024-04-05 20:24:27 -0400

    Idade
    Aula05.04

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int idade;
+    float altura;
+
+    cin >> idade;
+    cin >> altura;
+
+    cout << "idade: " << idade << endl;
+    cout << "Altura: " << altura << endl;
+
+    return 0;
+}
